2e00dbdc0e10c5ade4395e65ba6abd5353337ca0
nilhy <coderhaiyang@gmail.com>
2019-06-27 22:40:21 +0800

    fix navigationBar margin

--- Example/YHKit/ViewController.swift
+++ Example/YHKit/ViewController.swift
@@ -11,6 +11,7 @@ import YHKit
 
 class ViewController: YHViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,22 +21,32 @@ class ViewController: YHViewController {
         view.enableDebug = true
         
         let button = UIButton(title: "第一个", target: self, action: #selector(test))
+        button.backgroundColor = UIColor.random()
         /// 默认有一个返回不喜欢可以直接替换
         //yh_navigationBar.yh_leftBarButtonItems.append(button)
         yh_navigationBar.yh_leftBarButtonItems = [button]
         
         let button2 = UIButton(title: "第二个", target: self, action: #selector(test))
+        button2.backgroundColor = UIColor.random()
         button2.frame.size.width = 80
         yh_navigationBar.yh_leftBarButtonItems.append(button2)
         
         let button3 = UIButton(title: "第三个", target: self, action: #selector(test))
-        button3.frame.size.width = 60
+        button3.backgroundColor = UIColor.random()
+        let button4 = UIButton(title: "第四个", target: self, action: #selector(test))
+        button4.backgroundColor = UIColor.random()
+
+        //button3.frame.size.width = 60
         yh_navigationBar.yh_rightBarButtonItems.append(button3)
+        yh_navigationBar.yh_rightBarButtonItems.append(button4)
         
     }
 
     @objc func test() {
         
+        let vc = YHWebViewController()
+        vc.gotoUrl = "http://baidu.com"
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     override func didReceiveMemoryWarning() {
